09d4fef44cc57a1b9f50af60f3a8ae68b6524eab
Hack Reactor Student <student@hackreactor.com>
2017-04-13 14:40:39 -0700

    Pass tests through searchYouTube

--- src/data/exampleVideoData.js
+++ src/data/exampleVideoData.js
@@ -7,10 +7,11 @@ window.exampleVideoData = [{
   id: {
     kind: 'youtube#video',
     videoId: '4ZAEBxGipoA'
+    channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
   },
   snippet: {
-    publishedAt: '2015-08-02T20:52:58.000Z',
     channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
+    publishedAt: '2015-08-02T20:52:58.000Z',
     title: 'React JS Tutorial for Beginners - 1 - Introduction',
     description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
     thumbnails: {
@@ -39,10 +40,11 @@ window.exampleVideoData = [{
   id: {
     kind: 'youtube#video',
     videoId: 'mFEoarLnnqM'
+    channelId: 'UCKKQaFUwyPb6iC-42rFRz2Q',
   },
   snippet: {
-    publishedAt: '2014-08-08T21:31:58.000Z',
     channelId: 'UCKKQaFUwyPb6iC-42rFRz2Q',
+    publishedAt: '2014-08-08T21:31:58.000Z',
     title: 'Thinking in React, a step by step screencast tutorial',
     description: 'A step by step video tutorial that takes you through the correct way of coding Facebook React views. Check out the rest of the tagtree library: http://tagtree.tv.',
     thumbnails: {
@@ -71,10 +73,11 @@ window.exampleVideoData = [{
   id: {
     kind: 'youtube#video',
     videoId: '0ByoQm-vnYw'
+    channelId: 'UClLXKYEEM8OBBx85DOa6-cg',
   },
   snippet: {
-    publishedAt: '2015-12-08T20:51:18.000Z',
     channelId: 'UClLXKYEEM8OBBx85DOa6-cg',
+    publishedAt: '2015-12-08T20:51:18.000Z',
     title: 'React Tutorial for Beginners',
     description: 'Get the full course here: http://devslopes.com/react In this video you will learn how to setup your project for React development and you will build a simple React ...',
     thumbnails: {
@@ -103,10 +106,11 @@ window.exampleVideoData = [{
   id: {
     kind: 'youtube#video',
     videoId: 'o5E894TmHJg'
+    channelId: 'UCMYct3vtNaMwZAA3_DNsWMw',
   },
   snippet: {
-    publishedAt: '2015-03-09T08:19:59.000Z',
     channelId: 'UCMYct3vtNaMwZAA3_DNsWMw',
+    publishedAt: '2015-03-09T08:19:59.000Z',
     title: 'Flux Tutorial - Writing a Simple App in Flux and React',
     description: 'Hey! Write something in the comments, even if it\'s critical. I\'d love to hear feedback! (also, please thumbs-up if you like the vid, and I\'ll make more). Personally I ...',
     thumbnails: {
@@ -135,10 +139,11 @@ window.exampleVideoData = [{
   id: {
     kind: 'youtube#video',
     videoId: 'Pd6Ub7Ju2RM'
+    channelId: 'UCr5lOCcjZzNprLrhxO0WZQw',
   },
   snippet: {
-    publishedAt: '2015-02-13T18:50:38.000Z',
     channelId: 'UCr5lOCcjZzNprLrhxO0WZQw',
+    publishedAt: '2015-02-13T18:50:38.000Z',
     title: 'Learn React, Flux, and Flow: Part I',
     description: 'Brought to you by Formidable Labs and SeattleJS, Colin Megill walks us through Facebook\'s React framework in part one of this three-part series. The workshop ...',
     thumbnails: {
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var youtubeUrl = "https://www.googleapis.com/youtube/v3/search";
+
+  $.ajax({
+  	url: youtubeUrl,
+  	type: 'GET',
+  	data: {key: options.key,
+  		  	maxResults: options.max,
+	  		  q: options.query,
+	  			part: 'snippet'},
+  	videoEmbeddable: true,
+  	contentType: 'application/json',
+  	// videoType: 'any',
+  	success: (data) => {
+  		console.log("data received");
+  		console.log(data.items);
+  		callback(data.items);
+  	},
+  	error: () => {console.log("fail")}
+  })
 };
 
 window.searchYouTube = searchYouTube;
